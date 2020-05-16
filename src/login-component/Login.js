210e1bfaeb55a65e4c2264861d882d4dff2f02f3
jakubwilk <jw@jakubwilk.pl>
2020-05-16 19:40:19 +0200

    Basic style in login page

--- src/login-component/Login.js
+++ src/login-component/Login.js
@@ -1,5 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import background from "../utils/auth-background.jpg";
+
 import LoginForm from "./LoginForm";
+
+const LoginPage = styled.div`
+    background-image: url(${props => props.background});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100vh;
+    padding-top: 70px;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    
+    @media screen and (min-width: 768px) {
+        max-width: 650px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    
+    @media screen and (min-width: 960px) {
+        padding-left: 0;
+        padding-right: 0;
+    }
+`;
+
+const FormContent = styled.section`
+    background-color: white;
+    box-shadow: 10px 10px 0 #f2f2f2;
+    padding: 1.5rem;
+`;
 
 class Login extends React.Component {
     state = {
@@ -35,14 +70,22 @@ class Login extends React.Component {
     }
 
     render() {
-        return <LoginForm 
-                email={this.state.email} 
-                password={this.state.password} 
-                updateState={this.updateState} 
-                resType={this.state.data.type} 
-                resMessage={this.state.data.message}
-                submitMethod={this.fetchData}
-                loading={this.state.loading} />;
+        return (
+            <LoginPage background={background}>
+                <Container>
+                    <FormContent>
+                        <LoginForm 
+                            email={this.state.email} 
+                            password={this.state.password} 
+                            updateState={this.updateState} 
+                            resType={this.state.data.type} 
+                            resMessage={this.state.data.message}
+                            submitMethod={this.fetchData}
+                            loading={this.state.loading} />
+                    </FormContent>
+                </Container>
+            </LoginPage>
+        );
     }
 }
 
